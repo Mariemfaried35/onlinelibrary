409466a5a9cf179494b32ffb9694b9f385c3d107
Mariemfaried35 <98717566+Mariemfaried35@users.noreply.github.com>
2024-04-13 04:22:52 +0200

    Program.cs

--- OlineLibrary/Program.cs
+++ OlineLibrary/Program.cs
@@ -15,6 +15,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<OnlineLibraryContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineLibrary")));
+builder.Services.AddCors(P => P.AddPolicy("CorsPolicy",build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
@@ -24,7 +28,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
